8e2a2d16f6d47eed0a1bde652159fa0f20bdffca
Art Deineka <github@darkruby.co.uk>
2015-12-11 18:31:51 +0000

    added persistence

--- app/src/main/java/uk/co/darkruby/bbuddy/birthdaybuddy/MainActivity.java
+++ app/src/main/java/uk/co/darkruby/bbuddy/birthdaybuddy/MainActivity.java
@@ -1,6 +1,7 @@
 package uk.co.darkruby.bbuddy.birthdaybuddy;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -8,11 +9,25 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int ADD_PERSON = 1;
+    private final String BUDDIES_KEY = "buddies";
+    private SharedPreferences prefs;
+    private SharedPreferences.Editor prefEditor;
+    private Gson gson;
+    private ArrayList<BuddyModel> buddiesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +44,24 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(addPersonIntent, ADD_PERSON);
             }
         });
+
+        this.prefs = getPreferences(MODE_PRIVATE);
+        this.prefEditor = this.prefs.edit();
+        this.gson = new Gson();
+
+        this.initializeBuddiesList();
+
+        ListView listView = (ListView) findViewById(R.id.main_list);
+        ArrayAdapter<BuddyModel> adapter = new ArrayAdapter<>(MainActivity.this, R.layout.buddy_list_item, this.buddiesList);
+//        listView.setAdapter(adapter);
+
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == ADD_PERSON) {
             if (resultCode == RESULT_OK) {
                 BuddyModel buddy = (BuddyModel) data.getSerializableExtra("buddy");
-                Toast.makeText(MainActivity.this, buddy.toString(), Toast.LENGTH_SHORT)
-                    .show();
+                MainActivity.this.addToBuddiesList(buddy);
             }
         }
     }
@@ -62,4 +87,30 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void initializeBuddiesList() {
+        this.buddiesList = new ArrayList<>();
+        String buddiesListString = this.prefs.getString(BUDDIES_KEY, this.gson.toJson(this.buddiesList));
+        Type type = new TypeToken<ArrayList<BuddyModel>>(){}.getType();
+        ArrayList<BuddyModel> items = this.gson.fromJson(buddiesListString, type);
+        this.buddiesList.addAll(items);
+    }
+
+    private void addToBuddiesList(BuddyModel buddy, boolean persist) {
+        this.buddiesList.add(buddy);
+        if (persist) {
+            this.persistBuddiesList();
+        }
+    }
+
+    private void persistBuddiesList() {
+        this.prefEditor
+            .putString(BUDDIES_KEY, this.gson.toJson(this.buddiesList))
+            .apply();
+    }
+
+    private void addToBuddiesList(BuddyModel buddy) {
+        this.addToBuddiesList(buddy, true);
+    }
+
 }
